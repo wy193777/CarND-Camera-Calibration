944ad6d7c083ee9093959c27823a8ca04197164b
Shenghan Gao <gaoshenghan199123@gmail.com>
2017-09-04 00:35:53 -0700

    genereated images undistorted and perspective transforred

--- lane_lines.py
+++ lane_lines.py
@@ -3,6 +3,7 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 from scipy import misc
+from os import path
 
 
 def calibrate_camera(fnames):
@@ -75,6 +76,28 @@ def edge_detect(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     return color_binary, combined_binary
 
 
+def perspective_trans(img):
+    w, h = 1280, 720
+    x, y = 0.5*w, 0.8*h
+    src = np.float32([
+        [200./1280*w, 720./720*h],
+        [453./1280*w, 547./720*h],
+        [835./1280*w, 547./720*h],
+        [1100./1280*w, 720./720*h]])
+    dst = np.float32([
+        [(w-x)/2., h],
+        [(w-x)/2., 0.82*h],
+        [(w+x)/2., 0.82*h],
+        [(w+x)/2., h]])
+    # Compute and apply perpective transform
+    img_size = (img.shape[1], img.shape[0])
+    M = cv2.getPerspectiveTransform(src, dst)
+    warped = cv2.warpPerspective(
+        img, M, img_size, flags=cv2.INTER_NEAREST)
+
+    return warped
+
+
 def draw_undistord():
     objpoints, imgpoints = calibrate_camera(glob.glob('camera_cal/*.jpg'))
     image = misc.imread('camera_cal/calibration1.jpg')
@@ -90,7 +113,7 @@ def draw_undistord():
 
 
 def draw_edges():
-    image = misc.imread('test_images/test1.jpg')
+    image = misc.imread('test_images/straight_lines2.jpg')
     result = edge_detect(image)
 
     # Plot the result
@@ -109,6 +132,33 @@ def draw_edges():
     plt.show()
 
 
+def draw_perspective():
+    image = misc.imread('test_images/straight_lines1.jpg')
+    result = perspective_trans(image)
+
+    # Plot the result
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    f.tight_layout()
+
+    ax1.imshow(image)
+    ax1.set_title('Original Image', fontsize=40)
+
+    ax2.imshow(result)
+    ax2.set_title('Colorted Result', fontsize=40)
+
+    plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    plt.show()
+
+
+def generate_images(in_fnames, out_directory, func):
+    for fname in in_fnames:
+        image = misc.imread(fname)
+        save_path = path.join(out_directory, path.basename(fname))
+        misc.imsave(save_path, func(image))
+
 if __name__ == "__main__":
     # draw_undistord()
-    draw_edges()
+    # draw_edges()
+    # draw_perspective()
+    generate_images(
+        glob.glob('test_images/*.jpg'), 'output_images/test_images', perspective_trans)
